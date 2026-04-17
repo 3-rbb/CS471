8a823fa1675f99e223fbc2038b334bfd6da35170
Sultan <sultanm7md3322@gmail.com>
2026-04-17 14:29:56 +0300

    Initial commit - Django project

--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.bookmodule',
+   
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
